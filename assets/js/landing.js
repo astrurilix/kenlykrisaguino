48bddf331d1bf18407db1b3037a98000e0fa3d44
Kenly <kenlykrisaguinosantoso10@gmail.com>
2023-12-08 08:47:26 +0700

    Updated Landing Page

---
+++ assets/js/landing.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    // Define the skills array
+    var skillsArray = ["FULLSTACK WEB DEVELOPER", "BACK-END WEB DEVELOPER", "LARAVEL DEVELOPER"];
+    var currentIndex = 0;
+
+    // Function to update landing skills
+    function updateLandingSkills() {
+      var landingSkills = $("#landing-skills");
+      var currentSkill = skillsArray[currentIndex];
+      var index = 0;
+
+      // Function to add a letter to landing skills
+      function addLetter() {
+        landingSkills.text(currentSkill.slice(0, index++));
+        if (index <= currentSkill.length) {
+          // If not reached the end of the array, continue adding letters
+          setTimeout(addLetter, 100);
+        } else {
+          // If reached the end, pause for 3 seconds and then start deleting letters
+          setTimeout(deleteLetter, 3000);
+        }
+      }
+
+      // Function to delete a letter from landing skills
+      function deleteLetter() {
+        landingSkills.text(currentSkill.slice(0, index--));
+        if (index >= 0) {
+          // If not reached the beginning of the array, continue deleting letters
+          setTimeout(deleteLetter, 100);
+        } else {
+          // If reached the beginning, move to the next array element
+          currentIndex = (currentIndex + 1) % skillsArray.length;
+          // Start updating skills for the next array element
+          setTimeout(updateLandingSkills, 1000);
+        }
+      }
+
+      // Start the process by adding the first letter
+      addLetter();
+    }
+
+    function animateKenlyKrisaguino() {
+      var image = $("#landing-img")
+      function animate1() {
+        image.css('transform', "rotate(.5deg)")
+        setTimeout(animate2, 1000);
+      }
+      function animate2() {
+        image.css('transform', "rotate(-1deg)")
+        setTimeout(animate1, 1000);
+      }
+      animate1()
+    }
+
+    // Call the function when the document is ready
+    updateLandingSkills();
+    animateKenlyKrisaguino()
+  });
--- assets/js/section.js
+++ assets/js/section.js
@@ -1,53 +1,55 @@
-$(document).ready(function() {
-    // Cache jQuery selections
-    var navLinks = $('nav a');
-    var dotNavbar = $('#dot-navbar');
-  
-    // Function to update the dot position
-    function updateDotPosition(link) {
-      var linkPosition = link.position().left + link.width() / 2;
-      dotNavbar.css('transform', 'translateX(' + linkPosition + 'px)');
-    }
-  
-    // Function to handle scroll and resize events
-    function handleScrollAndResize() {
-      // Get the current scroll position
-      var scrollPosition = $(window).scrollTop();
-  
-      // Flag to determine if the active section is found
-      var activeSectionFound = false;
-  
-      // Loop through each section
-      $('section').each(function() {
-        if (activeSectionFound) return; // Skip the loop if the active section is found
-  
-        var sectionTop = $(this).offset().top;
-        var sectionHeight = $(this).outerHeight();
-  
-        // Check if the user has scrolled to at least 51% of the section
-        if (scrollPosition >= sectionTop && scrollPosition <= sectionTop + sectionHeight * 0.49) {
-          // Remove 'active' class from all navigation links
-          navLinks.removeClass('active-link');
-  
-          // Add 'active' class to the corresponding navigation link
-          var activeLink = navLinks.filter('[href="#' + $(this).attr('id') + '"]');
-          activeLink.addClass('active-link');
-  
-          // Update the dot position
-          updateDotPosition(activeLink);
-  
-          // Set the flag to true since the active section is found
-          activeSectionFound = true;
-        }
-      });
-    }
-  
-    // Call the function on page load
-    handleScrollAndResize();
-  
-    // Event listener for scroll and resize events
-    $(window).on('scroll resize', function() {
-      handleScrollAndResize();
+$(document).ready(function () {
+  var navLinks = $('nav a');
+  var dotNavbar = $('#dot-navbar');
+
+  function updateDotPosition(link) {
+    var linkPosition = link.position().left + link.width() / 2;
+    dotNavbar.css('transform', 'translateX(' + linkPosition + 'px)');
+  }
+
+  function handleScrollAndResize() {
+    var scrollPosition = $(window).scrollTop();
+    var activeLink;
+
+    $('section').each(function () {
+      var section = $(this);
+      var sectionTop = section.offset().top;
+      var sectionHeight = section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition <= sectionTop + sectionHeight * 0.9) {
+        activeLink = navLinks.filter('[href="#' + section.attr('id') + '"]');
+        return false; // Break the loop once the active section is found
+      }
     });
-  });
-  
+
+    if (activeLink && !activeLink.hasClass('active-link')) {
+      navLinks.removeClass('active-link');
+      activeLink.addClass('active-link');
+      updateDotPosition(activeLink);
+    }
+  }
+
+  function handleNavLinkClick(event) {
+    event.preventDefault();
+
+    var targetId = $(this).attr('href');
+    var targetSection = $(targetId);
+
+    $('html, body').animate({
+      scrollTop: targetSection.offset().top
+    }, 75, function () {
+      handleScrollAndResize(); // Call the scroll handler after the animation is complete
+      $(window).trigger('resize'); // Trigger the resize event manually
+    });
+  }
+
+  // Update dot position on initial load
+  handleScrollAndResize();
+
+  // Update dot position on scroll and resize
+  $(window).on('resize', handleScrollAndResize);
+  $(window).on('scroll', handleScrollAndResize);
+
+  // Update dot position on nav link click
+  navLinks.on('click', handleNavLinkClick);
+});
